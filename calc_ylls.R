f11e2b4cec8a32d3344ee485517bfd25282463ef
MED\mmc33 <matthew_coates@hms.harvard.edu>
2017-03-02 17:30:22 -0500

    explore how counted calculates rates on site

--- calc_ylls.R
+++ calc_ylls.R
@@ -15,26 +15,77 @@ d[age=="Unknown",age:=NA]
 d[age=="40s",age:="45"]
 d[,age:=as.numeric(age)]
 
-
-## read in standard life expectancy file
+## read in standard life expectancy file and format
 le <- fread(paste0(rootdir,"FINAL_min_pred_ex.csv"))
 setnames(le,"Pred_ex","ylls")
+
+## read in census file and format
+cen <- fread(paste0(rootdir,"/census.csv"))
+##cen <- melt(cen,id.vars=c("origin","race","sex","year"))
+## for now, just make all-age rates
+cen <- cen[,c("origin","race","sex","year","total_pop"),with=F]
+cen[sex==1 & origin==0 & year==2015 & race==8]$total_pop
+
+
+
 
 ## create YLLs
 d <- merge(d,le,by="age",all.x=T)
 if (nrow(d[!is.na(age) & is.na(ylls)]) > 0) stop("predex didn't merge for some ages")
-write.csv(d,paste0(rootdir,"counted_ylls.csv"),row.names=F)
+
+## create rates
+
+
+
+#write.csv(d,paste0(rootdir,"counted_ylls.csv"),row.names=F)
 
 ## tabulate age
 tab <- copy(d)
 tab[,n:=1]
-setkey(tab,gender,raceethnicity,armed)
+setkey(tab,raceethnicity,year)
 tab <- tab[,list(avg_age=mean(age,na.rm=T),n=sum(n),ylls=sum(ylls,na.rm=T)),by=key(tab)] ## FIXME: counting missings in n
 
 
+1000000*584/cen[sex==0 & origin==1 & year==2015 & race==1]$total_pop
+## White, non-hispanic, both sexes, "white alone" 2.944 compared to 2.95
+
+
+1000000*574/cen[sex==0 & origin==1 & year==2016 & race==1]$total_pop
+## White, non-hispanic, both sexes, "white alone" 2.89 compared to 2.9
+
+
+1000000*307/cen[sex==0 & origin==1 & year==2015 & race==2]$total_pop
+## black, non-hispanic, both sexes, "black alone" 7.717 compared to 7.69
+
+
+1000000*266/cen[sex==0 & origin==1 & year==2016 & race==2]$total_pop
+## black, non-hispanic, both sexes, "black alone" 6.627 compared to 6.66
+
+
+1000000*307/cen[sex==0 & origin==0 & year==2015 & race==2]$total_pop
+## black, total hispanic, both sexes, "black alone" 7.231 compared to 7.69
+
+
+1000000*266/cen[sex==0 & origin==0 & year==2016 & race==2]$total_pop
+## black, total hispanic, both sexes, "black alone" 6.204 compared to 6.66
+
+1000000*307/cen[sex==0 & origin==0 & year==2015 & race==8]$total_pop
+## black, total hispanic, both sexes, "black alone or combination" 6.656 compared to 7.69
+
+
+1000000*266/cen[sex==0 & origin==0 & year==2016 & race==8]$total_pop
+## black, total hispanic, both sexes, "black alone or combination" 5.698 compared to 6.66
+
+1000000*307/cen[sex==0 & origin==1 & year==2015 & race==8]$total_pop
+## black, not hispanic, both sexes, "black alone or combination" 7.19 compared to 7.69
+
+
+1000000*266/cen[sex==0 & origin==1 & year==2016 & race==8]$total_pop
+## black, not hispanic, both sexes, "black alone or combination" 6.161 compared to 6.66
+
 ##
-armed <- as.matrix(table(d$armed,d$raceethnicity))
-tots <- apply(armed,MARGIN=2,sum)
-test <- t(apply(armed,MARGIN=1,FUN=function(x){x/tots}))
+#armed <- as.matrix(table(d$armed,d$raceethnicity))
+#tots <- apply(armed,MARGIN=2,sum)
+#test <- t(apply(armed,MARGIN=1,FUN=function(x){x/tots}))
 
 
